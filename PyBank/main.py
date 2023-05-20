281d30fdd048f33e60341753fccdd33295e99fa4
Karina Quintana <kquintana031@gmail.com>
2023-05-20 17:06:16 -0600

    finalized challange

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,6 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
     csv_firstrow = next(csvreader)
     total += 1
     sum_PL += int(csv_firstrow[1])
@@ -24,14 +23,12 @@ with open(csvpath) as csvfile:
         avg.append(change)
         prev_amount=int(row[1])
         months.append(row[0])
-
-
-    
+ 
     avg_change=sum(avg)/len(avg)
     max_index=avg.index(max(avg))
     min_index=avg.index(min(avg))
 
-Bank_output=(f'Financial Analysis\n'
+bank_output=(f'Financial Analysis\n'
 f'----------------------------\n'
 f'Total Months: {total}\n'
 f'Total: ${sum_PL}\n'
@@ -39,7 +36,7 @@ f'Average Change: ${round(avg_change,2)}\n'
 f'Greatest Increase in Profits: {months[max_index]} (${max(avg)})\n'
 f'Greatest Decrease in Profits: {months[min_index]} (${min(avg)})')
                                       
-print(Bank_output)
+print(bank_output)
 
 with open(outputpath,"w") as outputfile:
-    outputfile.write(Bank_output)
+    outputfile.write(bank_output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,33 +2,53 @@ import csv
 
 csvpath = "../PyPoll/Resources/election_data_v2.csv"
 outputpath = "../PyPoll/Analysis/election_analysis.txt"
-# total = 0
-# count_votes = 0
-# candidates_votecount = 0
+
+total_votes = 0
+candidates = {}
+winner = ""
+max_votes = 0
 
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.DictReader(csvfile, delimiter=',')
     print(csvreader)    
     csv_header = next(csvreader)
-        
-    # for row in csvreader:
-    #     candidates_list = {rows[2]:rows[0] for rows in csvreader}
-    #     candidates_votecount = int(row[0]) 
-        
-        
-    #     for row in csvreader:
-    #         total += 1
-    #         count_votes += candidates_list + (int(row[0]))
-                        
-            
-poll_output=(f'Election Results\n'
-f'-------------------------\n'
-f'Total Votes: {total}\n'
-f'{candidates_list}: {count_votes}\n'
-f'-------------------------')
+      
+    total_votes += 1
+    for row in csvreader:
+        total_votes += 1
+        individual = row['Candidate']
+        if individual in candidates:
+            candidates[individual] += 1
+        else:
+            candidates[individual] = 1
+     
+for individual, votes in candidates.items():
+    percentage = (votes / total_votes) * 100
+    formatted_percentage = "{:.3f}".format(percentage)
+    print(f"{individual}: {formatted_percentage}% ({votes})")
+    
+    # If statement to find the candidate with max votes
+    if votes > max_votes:
+        max_votes = votes
+        winner = individual
 
+# Generate and append poll output(s)
+poll_output = "Election Results\n"
+poll_output += "-------------------------\n"
+poll_output += f"Total Votes: {total_votes}\n"
+poll_output += "-------------------------\n"
+
+for individual, votes in candidates.items():
+    percentage = (votes / total_votes) * 100
+    formatted_percentage = "{:.3f}".format(percentage)
+    poll_output += f"{individual}: {formatted_percentage}% ({votes})\n"
+
+poll_output += "-------------------------\n"
+poll_output += f"Winner: {winner}\n"
+poll_output += "-------------------------\n"
+
+# Print and output results
 print(poll_output)
 
-with open(outputpath,"w") as outputfile:
+with open(outputpath, "w") as outputfile:
     outputfile.write(poll_output)
-
